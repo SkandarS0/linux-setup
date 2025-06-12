c1816f26619145daa373f6ce803d703c090115ec
Skandar Souissi <iskandarsouissi.0@gmail.com>
2025-06-12 17:42:23 +0100

    fix(dev-setup): standardize function naming to kebab-case

--- omz-custom/dev-setup.zsh
+++ omz-custom/dev-setup.zsh
@@ -1,17 +1,17 @@
-php_setup() {
+php-setup() {
     env PATH="$PATH" PHP_INI_SCAN_DIR="$PHP_INI_SCAN_DIR" bash -c "$(curl -fsSL https://php.new/install/linux/8.4 | sed s/uninstall_herd_lite/uninstall/g | sed s#\$HOME/.config/herd-lite/bin#\$HOME/.dev-tools/php#g)"
 }
 
-uv_setup() {
+uv-setup() {
     curl -LsSf https://astral.sh/uv/install.sh | env UV_INSTALL_DIR="$UV_INSTALL_DIR" INSTALLER_NO_MODIFY_PATH=1 bash
     env UV_PYTHON_INSTALL_DIR="$UV_INSTALL_DIR/python" "$UV_INSTALL_DIR/uv" python install
 }
 
-rust_setup() {
+rust-setup() {
     curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | env CARGO_HOME="$HOME/.dev-tools/cargo" RUSTUP_HOME="$HOME/.dev-tools/rustup" bash -s -- -y --no-modify-path --default-toolchain '1.87.0'
 }
 
-pnpm_setup() {
+pnpm-setup() {
     # download latest release from GitHub
     local url="https://github.com/pnpm/pnpm/releases/latest/download/pnpm-linux-x64"
     local dest="$HOME/.dev-tools/pnpm/pnpm"
@@ -20,6 +20,6 @@ pnpm_setup() {
     chmod +x "$dest"
 }
 
-sdkman_setup() {
+sdkman-setup() {
     curl -s "https://get.sdkman.io" | env SDKMAN_DIR="$HOME/.dev-tools/sdkman" bash
 }
